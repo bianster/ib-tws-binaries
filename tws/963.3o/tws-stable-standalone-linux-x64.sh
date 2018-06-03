33fb4f96cb0664721bd2368a6d83db04dc894ace
Douglas Tan <bianster@gmail.com>
2018-06-03 23:15:12 +0800

    IB Gateway 963.30 (Stable) & 972.1e (Latest)

---
+++ tws/963.3o/tws-stable-standalone-linux-x64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e2707193d13793ace9ff0d7c5769a17368636f656e29a2f0be1475e94be03dd
+size 77109885
---
+++ tws/972.1e/tws-latest-standalone-linux-x64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c5db486dc1e15f181728f8d1087421b3eacfafac0aed1c7382ae9660f9599d2a
+size 74263803
